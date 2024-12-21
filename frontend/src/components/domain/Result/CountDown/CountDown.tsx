145080ca1cd643c848cb77fdee4a95d02e101b36
Leestalion <thomas.lemarrec@wolterskluwer.com>
2024-12-21 16:35:32 +0100

    Fix import path for CountDown component styles to ensure correct styling

--- frontend/src/components/domain/Result/CountDown/CountDown.tsx
+++ frontend/src/components/domain/Result/CountDown/CountDown.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styles from "./Countdown.module.css";
+import styles from "./CountDown.module.css";
 
 export const CountDown = () => {
   const [timeRemaining, setTimeRemaining] = useState("");
